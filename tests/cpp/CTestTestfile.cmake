ff6b771919c6d418ecb3faeb233ce82942632f30
Mohammad Pervaiz <mdrpz04@gmail.com>
2026-01-16 22:23:07 +0700

    implemented sticky sessions for persistence

--- tests/cpp/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Mohammad Pervaiz/projects/loadbalancer/tests/cpp
-# Build directory: /mnt/c/Users/Mohammad Pervaiz/projects/loadbalancer/tests/cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[routing_tests]=] "/mnt/c/Users/Mohammad Pervaiz/projects/loadbalancer/tests/cpp/test_routing")
-set_tests_properties([=[routing_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/Mohammad Pervaiz/projects/loadbalancer/tests/cpp/CMakeLists.txt;6;add_test;/mnt/c/Users/Mohammad Pervaiz/projects/loadbalancer/tests/cpp/CMakeLists.txt;0;")
